a66d46cc8fbe45d2baa924efa5d8494cb15f21a8
Daiane Souza <dane.consultora@gmail.com>
2026-04-25 12:15:14 -0300

    concatenação de nome

---
+++ ConcatenarNome.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class ConcatenarNome
+{
+    public static void Executar()
+    {
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite seu sobrenome: ");
+        string sobrenome = Console.ReadLine();
+
+        Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+
+        Console.ReadKey();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,16 +3,7 @@
 class Program
 {
     static void Main()
-
-      {
-        // Solicita o nome do usuário
-        Console.Write("Digite seu nome: ");
-        string nome = Console.ReadLine();
-
-        // Exibe mensagem personalizada
-        Console.WriteLine($"Olá, {nome}! Seja muito bem-vindo!");
-
-        // Pausa para visualizar o resultado
-        Console.ReadKey();
+    {
+        ConcatenarNome.Executar();
     }
 }
