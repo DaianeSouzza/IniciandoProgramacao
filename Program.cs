3d6b8ac5246ecd3693cb1aba7d8b795ff7f3ae93
Daiane Souza <dane.consultora@gmail.com>
2026-04-25 11:56:00 -0300

    Primeiro commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+
+      {
+        // Solicita o nome do usuário
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        // Exibe mensagem personalizada
+        Console.WriteLine($"Olá, {nome}! Seja muito bem-vindo!");
+
+        // Pausa para visualizar o resultado
+        Console.ReadKey();
+    }
+}
